2082003bbc54f755e351911a7a8ccad71455be32
Ivan Esin <ivan.esin.78@mail.ru>
2024-11-29 10:27:58 +0400

    final commit

--- testTaskOnboarding/src/firebase/firebaseConfig.ts
+++ testTaskOnboarding/src/firebase/firebaseConfig.ts
@@ -1,19 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { getAnalytics,logEvent  } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBp5ZMR9wqE0kwjQ1n1C640bbMF9KfRqJY",
-  authDomain: "onboarding-9c7cf.firebaseapp.com",
-  projectId: "onboarding-9c7cf",
-  storageBucket: "onboarding-9c7cf.firebasestorage.app",
-  messagingSenderId: "131418997790",
-  appId: "1:131418997790:web:81a0271fc1392ae0499a0f",
-  measurementId: "G-J5Z7VH6JHN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
-export {auth, analytics,logEvent, createUserWithEmailAndPassword, signInWithEmailAndPassword }
+export { auth, analytics, logEvent, createUserWithEmailAndPassword, signInWithEmailAndPassword }
